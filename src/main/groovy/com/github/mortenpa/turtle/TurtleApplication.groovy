6fd15aae38eeb197502c153bd873a0c39ebdce26
Morten <mortenpaluteder@github.com>
2024-11-04 13:15:14 +0200

    initial commit

---
+++ src/main/groovy/com/github/mortenpa/turtle/TurtleApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.mortenpa.turtle
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TurtleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TurtleApplication, args)
+    }
+
+}
